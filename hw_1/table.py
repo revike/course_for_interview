9751ba179531f2d54350df8b79e96d0fd9d55cf0
revike <revike@ya.ru>
2021-04-07 04:14:22 +0300

    hw_1

--- hw_1/table.py
+++ hw_1/table.py
@@ -25,8 +25,8 @@ def multiplication_table_1(a, b):
     if isinstance(a, int) and isinstance(b, int):
         if a > 0 and b > 0:
             return table_logic_1(a, b)
-        return 'A и B должны быть целыми положительными числами'
-    return 'A и B должны быть целыми числами!'
+        raise ValueError('A и B должны быть целыми положительными числами')
+    raise ValueError('A и B должны быть целыми числами!')
 
 
 # =================================================
@@ -51,8 +51,8 @@ def multiplication_table_2(a, b):
     if isinstance(a, int) and isinstance(b, int):
         if a > 0 and b > 0:
             return table_logic_2(a, b)
-        return 'A и B должны быть целыми положительными числами'
-    return 'A и B должны быть целыми числами!'
+        raise ValueError('A и B должны быть целыми положительными числами')
+    raise ValueError('A и B должны быть целыми числами!')
 
 
 result_1 = multiplication_table_1(5, 5)
